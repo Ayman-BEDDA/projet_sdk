e447e638440c66b5cab3751824b4702de84da2dc
simonBtssio <simon.farnault.sio@gmail.com>
2022-06-22 12:26:33 +0200

    Connection discord ok

--- client/index.php
+++ client/index.php
@@ -2,6 +2,8 @@
 
 function login()
 {
+    //url pour accéder à la connection discord
+    $discord_url = "https://discord.com/api/oauth2/authorize?client_id=988725359518294016&redirect_uri=http%3A%2F%2Flocalhost%3A8081%2Fds_callback&response_type=code&scope=identify%20guilds";
     $queryParams= http_build_query(array(
         "client_id" => "621e3b8d1f964",
         "redirect_uri" => "http://localhost:8081/callback",
@@ -24,7 +26,9 @@ function login()
         "scope" => "public_profile,email",
         "state" => bin2hex(random_bytes(16))
     ));
-    echo "<a href=\"https://www.facebook.com/v2.10/dialog/oauth?{$queryParams}\">Se connecter via Facebook</a>";
+    echo "<a href=\"https://www.facebook.com/v2.10/dialog/oauth?{$queryParams}\">Se connecter via Facebook</a><br>";
+    //lien de connection à discord
+    echo "<a href=\"" . $discord_url . "\">Connection via Discord</a>";
 }
 
 function callback()
@@ -99,6 +103,40 @@ function fbcallback()
     echo "Hello {$result['name']}";
 }
 
+function dscallback(){
+    //Verif si le code de discord est présent
+    if(!isset($_GET['code'])){
+        echo 'no code';
+        die();
+    }
+    $ds_code = $_GET['code'];
+
+    $payload = [
+        'code' => $ds_code,
+        'client_id' => '988725359518294016',
+        'client_secret' => 'woE63_1JETD4Y3XXyKK6c5SXvJTJYEIx',
+        'grant_type' => 'authorization_code',
+        'redirect_uri' => 'http://localhost:8081/ds_callback',
+        'scope' => 'identify guids',
+    ];
+
+    $payload_str = http_build_query($payload);
+    $discord_token_url = "https://discord.com/api/v10/oauth2/token";
+
+    $options = [
+        'http' => [
+            'header'  => "Accept: application/json\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: " . strlen($payload_str) . "\r\n",
+            'method'  => 'POST',
+            'content' => $payload_str
+        ]
+    ];
+    $context  = stream_context_create($options);
+    $result = file_get_contents($discord_token_url, false, $context);
+    if ($result === FALSE) { 
+        echo "une erreur est survenue";
+    }
+}
+
 $route = $_SERVER['REQUEST_URI'];
 switch (strtok($route, "?")) {
     case '/login':
@@ -109,6 +147,9 @@ switch (strtok($route, "?")) {
         break;
     case '/fb_callback':
         fbcallback();
+        break;
+    case '/ds_callback':
+        dscallback();
         break;
     default:
         echo '404';
